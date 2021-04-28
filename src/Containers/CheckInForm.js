b8bb68d3945262e44fecf7135bed2f6a5800c4d5
Eunice Soh <eunice.sjy@gmail.com>
2021-04-28 22:43:54 +0800

    [EJYS] Add forms

--- src/Containers/CheckInForm.js
+++ src/Containers/CheckInForm.js
@@ -1,51 +1,80 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { Home } from 'grommet-icons';
 
 import { 
   Box, Heading, 
   FormField, Form, TextInput,
-  Button,
+  Button, Paragraph,
 } from 'grommet';
 
-const optsAxios = {
-  headers: {
-    'Access-Control-Allow-Origin':'*' ,
-  },
+import Spinner from  '../Components/Spinner';
+
+const opts = {
+  'Access-Control-Allow-Origin':'*' ,
+  'Content-Type': 'text/plain;charset=utf-8',
 };
 
 class CheckInForm extends Component {
 
   state = {
     checkingIn: false,
-  }
-
-  componentDidMount() {
-    console.log(process.env.REACT_APP_GOOGLE_FORM_KEY);
+    message: null,
   }
 
   submitFormHandler = (event) => {
     event.preventDefault();
+    this.setState({checkingIn: true});
 
-    console.log('[submitFormHandler]');
-    console.log(process.env.REACT_APP_GOOGLE_FORM_KEY);
-    console.log(event.value);
-
-    const base = 'https://script.google.com/macros/s/';
-    const url = `${base}${process.env.REACT_APP_GOOGLE_FORM_KEY}/exec`;
+    const base = process.env.REACT_APP_GOOGLE_FORM_BASE_URL;
+    const url = `${base}${process.env.REACT_APP_GOOGLE_FORM_KEY_CHECKIN}/exec`;
     const params = Object.entries(event.value)
       .map(([key, val]) => `${key}=${val}`).join('&');
 
-    axios.post(`${url}?${params}`, optsAxios)
-      .then(response => {
-        console.log('[response.data]', response.data);
+    fetch(`${url}?${params}`, {method: 'POST'})
+      .then(_response => {
+        this.setState({checkingIn: false, message: 'Successfully submitted'});
       })
       .catch(error => {
-        console.log('[error]', error);
+        console.log('[Check in error]', error);
+        this.setState({checkingIn: false, message: 'Failed to submit'});
       });
+    
+    setTimeout(() => {
+      this.setState({message: null});
+    }, 4000);
   } 
 
   render() {
+
+
+    const form = this.state.checkingIn ? <Spinner/> : (
+      <Form
+        onSubmit={this.submitFormHandler}
+      >
+        <Box direction="column">
+          <FormField name="first_name" label="First name" required>
+            <TextInput id="first_name" name="first_name" />
+          </FormField>
+
+          <FormField name="last_name" label="Last name" required>
+            <TextInput id="last_name" name="last_name" />
+          </FormField>
+        </Box>
+
+        <FormField name="contact_number" label="Contact number" required
+          validate={{ 
+            regexp: new RegExp(/^\d{8}$/), 
+            message: 'Your phone number should be 8 digits long', 
+          }}>
+          <TextInput id="contact_number" name="contact_number"/>
+        </FormField>
+
+        <Box alignContent="center" pad="medium">
+          <Button type="submit" primary label="Submit"/>
+        </Box>
+  
+      </Form>
+    );
+
     return(
       <Box flex={false} >
         <Box direction='row' justify='center'>
@@ -60,32 +89,11 @@ class CheckInForm extends Component {
               <Heading level="1" style={{padding: '0px'}}>
               Check In
               </Heading>
-              <Form
-                onSubmit={this.submitFormHandler}
-              >
-                <Box direction="column">
-                  <FormField name="first_name" label="First name" required>
-                    <TextInput id="first_name" name="first_name" />
-                  </FormField>
-
-                  <FormField name="last_name" label="Last name" required>
-                    <TextInput id="last_name" name="last_name" />
-                  </FormField>
-                </Box>
-
-                <FormField name="contact_number" label="Contact number" required
-                  validate={{ 
-                    regexp: new RegExp(/^\d{8}$/), 
-                    message: 'Your phone number should be 8 digits long', 
-                  }}>
-                  <TextInput id="contact_number" name="contact_number"/>
-                </FormField>
-
-                <Box alignContent="center" pad="medium">
-                  <Button type="submit" primary label="Submit"/>
-                </Box>
-            
-              </Form>
+              {this.state.message ? 
+                (<Paragraph size="small">{this.state.message}</Paragraph>)
+                : null
+              }
+              {form}
             </Box>
           </Box>
         </Box>
--- src/Containers/Guestbook.js
+++ src/Containers/Guestbook.js
@@ -3,12 +3,61 @@ import React, { Component } from 'react';
 import { 
   Box, Heading, 
   FormField, Form, TextInput, TextArea,
-  Button,
+  Button, Paragraph,
 } from 'grommet';
 
+import Spinner from '../Components/Spinner';
 class Guestbook extends Component {
 
+  state = {
+    submitting: false,
+    message: null,
+  }
+
+  submitFormHandler = (event) => {
+    event.preventDefault();
+    this.setState({submitting: true});
+
+    const base = process.env.REACT_APP_GOOGLE_FORM_BASE_URL;
+    const url = `${base}${process.env.REACT_APP_GOOGLE_FORM_KEY_GUESTBOOK}/exec`;
+    const params = Object.entries(event.value)
+      .map(([key, val]) => `${key}=${val}`).join('&');
+
+    fetch(`${url}?${params}`, {method: 'POST'})
+      .then(_response => {
+        this.setState({submitting: false, message: 'Thanks for your message!'});
+      })
+      .catch(error => {
+        console.log('[Guestbook error]', error);
+        this.setState({submitting: false, message: 'Failed to submit the message'});
+      });
+    
+    setTimeout(() => {
+      this.setState({message: null});
+    }, 4000);
+  }
+
   render() {
+
+
+    const form = this.state.submitting ? <Spinner/> : 
+      (          <Form onSubmit={this.submitFormHandler}>
+        <FormField name="name" htmlFor="text-input-id" label="Name(s)">
+          <TextInput id="text-input-id" name="names" />
+        </FormField>
+
+        <FormField name="message" htmlFor="text-input-id" label="Message">
+          <TextArea id="text-input-id" name="message" style={{
+            height: '150px',
+          }}/>
+        </FormField>
+
+        <Box direction="row" gap="medium" pad="medium">
+          <Button type="submit" primary label="Send your well wishes"/>
+        </Box>
+      
+      </Form>); 
+
     return(
       <Box flex={false} direction='row' justify='center'  pad={{top: '30px'}}>
         <Box 
@@ -20,31 +69,12 @@ class Guestbook extends Component {
           alignSelf='center'
         >
           <Heading level="1">
-                Guestbook 留言簿 
+            Guestbook 留言簿 
           </Heading>
-
-          <Form
-            // value={value}
-            // onChange={nextValue => setValue(nextValue)}
-            // onReset={() => setValue({})}
-            // onSubmit={({ value }) => {}}
-          >
-            <FormField name="name" htmlFor="text-input-id" label="Name">
-              <TextInput id="text-input-id" name="name" />
-            </FormField>
-
-            <FormField name="message" htmlFor="text-input-id" label="Message">
-              <TextArea id="text-input-id" name="message" style={{
-                height: '150px',
-              }}/>
-            </FormField>
-
-          </Form>
-
-          <Box direction="row" gap="medium" pad="medium">
-            <Button type="submit" primary label="Submit" />
-          </Box>
-
+          {this.state.message ? 
+            (<Paragraph size="small">{this.state.message}</Paragraph>)
+            : null}
+          {form}
         </Box>
       </Box>
     );
