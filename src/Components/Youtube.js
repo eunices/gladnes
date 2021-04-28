48c3c2511c31bd273762fb89642840352238a8ff
Eunice Soh <eunice.sjy@gmail.com>
2021-04-28 23:19:38 +0800

    [EJYS] Add video link

--- src/Components/Youtube.js
+++ src/Components/Youtube.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const Youtube = (embedId) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
+const Youtube = (embedId) => {
+
+  return( 
+
+    <div className="video-responsive">
+      <iframe
+        width="853"
+        height="480"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Youtube embedded"
+      />
+    </div>
+  );
+};
 
 // https://dev.to/bravemaster619/simplest-way-to-embed-a-youtube-video-in-your-react-app-3bk2
 
--- src/Containers/Livestream.js
+++ src/Containers/Livestream.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Box, Heading, Text } from 'grommet';
+import { Box, Heading, Text, Paragraph } from 'grommet';
 import { Link } from 'react-router-dom';
 
 import Youtube from '../Components/Youtube';
@@ -9,10 +9,17 @@ class Livestream extends Component {
   
   render() {
     const VIDEO_ID = 'iHbdzRCnPC8';
-
+    const url = `https://youtube.com/watch?v=${VIDEO_ID}`;
+    
     return(
       <Box fill>
+
         <Youtube embedId={VIDEO_ID}/>
+        <Box style={{margin: 'auto', textAlign: 'center'}}>
+          <Paragraph size="small"> 
+            If the embedded video does not work, use <a href={url}>{url}</a>
+          </Paragraph>
+        </Box>
       </Box>
         
     );
